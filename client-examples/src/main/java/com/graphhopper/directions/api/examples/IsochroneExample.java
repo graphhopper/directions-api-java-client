757633b184de8095697e3fde5055d062bea8faed
Peter <graphhopper@gmx.de>
2017-05-24 00:15:12 +0200

    added geocoding and isochrone examples

---
+++ client-examples/src/main/java/com/graphhopper/directions/api/examples/IsochroneExample.java
@@ -0,0 +1,35 @@
+package com.graphhopper.directions.api.examples;
+
+import com.graphhopper.directions.api.client.api.IsochroneApi;
+import com.graphhopper.directions.api.client.model.IsochroneResponse;
+import com.graphhopper.directions.api.client.model.ResponseCoordinatesArray;
+
+import java.util.List;
+
+/**
+ * A simple Isochrone example
+ */
+public class IsochroneExample {
+    public static void main(String[] args) {
+        new IsochroneExample().start();
+    }
+
+    private void start() {
+        IsochroneApi isochrone = new IsochroneApi();
+        String key = System.getProperty("graphhopper.key", "");
+        try {
+            // Please note: the request string for the point has the order "lat,lon" but the response contains
+            // an array with the order [lon,lat]
+            IsochroneResponse rsp = isochrone.isochroneGet("51.183728,14.42801", key, 10 * 60, -1, "car",
+                    3, false);
+            ResponseCoordinatesArray coords = rsp.getPolygons().get(0).getGeometry().getCoordinates();
+            System.out.println(coords);
+            List coords0 = (List) coords.get(0).get(0);
+            double lon0 = ((Number) coords0.get(0)).doubleValue();
+            double lat0 = ((Number) coords0.get(1)).doubleValue();
+            System.out.println("first coord " + lat0 + ", " + lon0);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
