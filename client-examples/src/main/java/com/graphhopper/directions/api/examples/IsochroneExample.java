3a4d15e1d73b06f70e38317569a265ebbbed1c13
Michael Zilske <michael.zilske@tu-berlin.de>
2017-07-25 16:14:01 +0200

    A polygon has an _array_ of coordinate _arrays_, not just one.

--- client-examples/src/main/java/com/graphhopper/directions/api/examples/IsochroneExample.java
+++ client-examples/src/main/java/com/graphhopper/directions/api/examples/IsochroneExample.java
@@ -2,6 +2,7 @@ package com.graphhopper.directions.api.examples;
 
 import com.graphhopper.directions.api.client.api.IsochroneApi;
 import com.graphhopper.directions.api.client.model.IsochroneResponse;
+import com.graphhopper.directions.api.client.model.IsochroneResponsePolygon;
 import com.graphhopper.directions.api.client.model.ResponseCoordinatesArray;
 
 import java.util.List;
@@ -16,17 +17,17 @@ public class IsochroneExample {
 
     private void start() {
         IsochroneApi isochrone = new IsochroneApi();
-        String key = System.getProperty("graphhopper.key", "");
+        String key = System.getProperty("graphhopper.key", "0ae2a909-38e7-4d3c-a315-9478e0c6399d");
         try {
             // Please note: the request string for the point has the order "lat,lon" but the response contains
             // an array with the order [lon,lat]
             IsochroneResponse rsp = isochrone.isochroneGet("51.183728,14.42801", key, 10 * 60, -1, "car",
                     3, false);
-            ResponseCoordinatesArray coords = rsp.getPolygons().get(0).getGeometry().getCoordinates();
-            System.out.println(coords);
-            List coords0 = (List) coords.get(0).get(0);
-            double lon0 = ((Number) coords0.get(0)).doubleValue();
-            double lat0 = ((Number) coords0.get(1)).doubleValue();
+            final IsochroneResponsePolygon isochrone0 = rsp.getPolygons().get(0);
+            ResponseCoordinatesArray exteriorRing = isochrone0.getGeometry().getCoordinates().get(0);
+            System.out.println(exteriorRing);
+            double lon0 = ((Number) exteriorRing.get(0).get(0)).doubleValue();
+            double lat0 = ((Number) exteriorRing.get(0).get(1)).doubleValue();
             System.out.println("first coord " + lat0 + ", " + lon0);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
